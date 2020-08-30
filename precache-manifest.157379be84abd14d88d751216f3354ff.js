53fe3110b2c99d6e6fb5b0cac3c49479845165ac
Badrri-Narayanan <badrri1995@gmail.com>
2020-08-30 17:06:54 +0530

    Updates

---
+++ precache-manifest.157379be84abd14d88d751216f3354ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d92762e3be92bf3a2d7fe87fcbc2cea",
+    "url": "/Badrri-Narayanan/petstore/index.html"
+  },
+  {
+    "revision": "1fa46b3e5bd230f36494",
+    "url": "/Badrri-Narayanan/petstore/static/css/main.72501848.chunk.css"
+  },
+  {
+    "revision": "25a8cc32b8b10ca1118d",
+    "url": "/Badrri-Narayanan/petstore/static/js/2.084f0f17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Badrri-Narayanan/petstore/static/js/2.084f0f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa46b3e5bd230f36494",
+    "url": "/Badrri-Narayanan/petstore/static/js/main.2aa861a4.chunk.js"
+  },
+  {
+    "revision": "6997ecda7fdc952da13b",
+    "url": "/Badrri-Narayanan/petstore/static/js/runtime-main.4d76f9df.js"
+  }
+]);
