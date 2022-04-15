c1528089a246ddbbb1db9402abf9e3cb47109dbc
Badrri Narayanan S <badrri1995@gmail.com>
2022-04-15 23:22:04 +0530

    made changes to index.js for upgrading to React 18 and updated circleci config

--- src/index.js
+++ src/index.js
@@ -16,14 +16,16 @@ const rootReducer = combineReducers({searchRobots, requestRobots})
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware ,logger));
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
